60a230fcd4777b2b3722ff634acf461ee0b81a62
Tsewang Rinzin <trinzin@nyc.yearup.org>
2018-11-19 16:49:25 -0500

    we are getting there

--- poke.js
+++ poke.js
@@ -1,49 +1,58 @@
 console.log('🐱‍👓');
 
 class pokemon{
-  constructor(name,pokeLife,pokeAttack,pokeDefense,pokeAbilities){
+  constructor(name,pokeLife,pokeAttack,pokeDefense,pokeAbilities,pic){
     this.name=name;
     this.pokeLife=pokeLife;
     this.pokeAttack=pokeAttack;
     this.pokeDefense=pokeDefense;
     this.pokeAbilities=pokeAbilities;
+    this.pic=pic;
 
     this.container = document.getElementById('products');
     //make a div with classname product//
     this.element = document.createElement('div');
     this.element.className='product';
 
+
+
 }
 
 display(){
 
-    let name = document.createElement("li");
+    let name = document.createElement("ol");
     name.innerHTML = this.name;
     this.element.appendChild(name);
 
     this.container.appendChild(this.element);
 
-    let pokeLife = document.createElement("li");
+    let pokeLife = document.createElement("ol");
     pokeLife.innerHTML = this.pokeLife;
     this.element.appendChild(pokeLife);
 
     this.container.appendChild(this.element);
 
-    let pokeAttack = document.createElement("li");
+    let pokeAttack = document.createElement("ol");
     pokeAttack.innerHTML = this.pokeAttack;
     this.element.appendChild(pokeAttack);
 
     this.container.appendChild(this.element);
 
-    let pokeDefense = document.createElement("li");
+    let pokeDefense = document.createElement("ol");
     pokeDefense.innerHTML = this.pokeDefense;
     this.element.appendChild(pokeDefense);
 
     this.container.appendChild(this.element);
 
-    let pokeAbilities = document.createElement("li");
+    let pokeAbilities = document.createElement("ol");
+    this.conatiner2 = document.getElementById('test')
     pokeAbilities.innerHTML = this.pokeAbilities;
-    this.element.appendChild(pokeAttack);
+    this.element.appendChild(pokeAbilities);
+
+    let pic=document.createElement('img');
+    pic.src=this.pic;
+    this.element.appendChild(pic);
+    // document.getElementsById('prodcts').appendChild(pic);
 
     this.container.appendChild(this.element);
   }
@@ -65,7 +74,9 @@ axios.get
 
       let defense = response. data.stats[3].base_stat;
 
-    let pikachu = new pokemon(name, hp, ability, attack, defense);
+      let pic = "pic1.jpg";
+
+    let pikachu = new pokemon(name, hp, ability, attack, defense,pic);
     pikachu.display();
 });
 
@@ -83,8 +94,10 @@ axios.get
 
       let defense = response. data.stats[3].base_stat;
 
-  let charmander = new pokemon(name, hp, ability, attack, defense);
+      let pic = "charmander1.jpg";
 
+  let charmander = new pokemon(name, hp, ability, attack, defense,pic);
+  charmander.display();
 });
 
 axios.get
@@ -101,6 +114,8 @@ axios.get
 
       let defense = response. data.stats[3].base_stat;
 
-  let skarmory = new pokemon(name, hp, ability, attack, defense);
+      let pic = "skarmory.png";
 
+  let skarmory = new pokemon(name, hp, ability, attack, defense,pic);
+      skarmory.display();
 });
